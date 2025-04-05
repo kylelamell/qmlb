594e4984e6d1ac697fa343967a9fc219f53cf708
Kyle Lamell <143363041+kylelamell@users.noreply.github.com>
2025-04-04 23:14:32 -0400

    matrix mult now works

--- matrix.cpp
+++ matrix.cpp
@@ -28,7 +28,7 @@ Matrix Matrix::add(Matrix& m2) {
 Matrix Matrix::sub(Matrix& m2) {
   matrix mat2 = m2.mat;
   if (mat.size() != mat2.size() || mat[0].size() != mat2[0].size()) {
-    throw runtime_error("Matrix::add: Matrix dimensions do not match.");
+    throw runtime_error("Matrix->add: Matrix dimensions do not match.");
   }
   
   vector<vector<CNum>> result(mat.size(), vector<CNum>(mat[0].size()));
@@ -43,7 +43,24 @@ Matrix Matrix::sub(Matrix& m2) {
 }
 
 Matrix Matrix::mult(Matrix& m2) {
-  return Matrix();
+  matrix mat2 = m2.mat;
+  if (mat[0].size() != mat2.size()) {
+    throw runtime_error("Matrix->mult: Inner matrix dimentions do not match.");
+  }
+
+  matrix result(mat.size(), vector<CNum>(mat2[0].size()));
+
+  for (int i = 0; i < mat.size(); i++) {
+    for (int j = 0; j < mat2[0].size(); j++) {
+      CNum sum;
+      for (int k = 0; k < mat[0].size(); k++) {
+        sum = sum.add(mat[i][k].mult(mat2[k][j]));
+      }
+      result[i][j] = sum;
+    }
+  }
+
+  return Matrix(result);
 }
 
 Matrix Matrix::compConj() {
