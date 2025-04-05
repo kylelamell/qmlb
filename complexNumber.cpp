70cd894fca147b13a19db74e06b3521b0c07a3ca
Kyle Lamell <143363041+kylelamell@users.noreply.github.com>
2025-04-04 23:07:17 -0400

    fixed multiplication function

--- complexNumber.cpp
+++ complexNumber.cpp
@@ -15,9 +15,13 @@ CNum CNum::sub(CNum& cn2) {
 }
 
 CNum CNum::mult(CNum& cn2) {
-  return CNum((real * cn2.real) - (comp * comp), (real * cn2.comp) + (comp * cn2.real));
+  return CNum((real * cn2.real) - (comp * cn2.comp), (real * cn2.comp) + (comp * cn2.real));
 }
 
 std::string CNum::print() {
-  return "[" + std::to_string(real) + "+" + std::to_string(comp) + "i]";
+  if (comp >= 0) {
+    return "[" + std::to_string(real) + "+" + std::to_string(comp) + "i]";
+  }
+
+  return "[" + std::to_string(real) + std::to_string(comp) + "i]";  
 }
