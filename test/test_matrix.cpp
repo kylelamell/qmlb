b158e03614f5ceeb74a714464c349ec81ec5c8a5
Kyle Lamell <143363041+kylelamell@users.noreply.github.com>
2025-04-15 08:12:06 -0400

    reformatted tests

--- test/test_matrix.cpp
+++ test/test_matrix.cpp
@@ -4,18 +4,18 @@
 // Matrix add(const Matrix& m2) const;
 TEST(MatrixTest, AddNormal) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(4,4), CNum(3,3)},{CNum(2,2), CNum(1,1)}};
-  Matrix M2(m2);
   matrix m3 = {{CNum(5,5), CNum(5,5)},{CNum(5,5), CNum(5,5)}};
-  Matrix M3(m3);
-  EXPECT_EQ(M1.add(M2), M3);
+  Matrix M1(m1);
+  Matrix M2(m2);
+  Matrix Expected(m3);
+  EXPECT_EQ(M1.add(M2), Expected);
 }
 
 TEST(MatrixTest, AddWrongDimension) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(1,1)},{CNum(2,2)}};
+  Matrix M1(m1);
   Matrix M2(m2);
   EXPECT_THROW(M1.add(M2), std::runtime_error);
 }
@@ -23,18 +23,18 @@ TEST(MatrixTest, AddWrongDimension) {
 // Matrix subtract(const Matrix& m2) const;
 TEST(MatrixTest, SubtractNormal) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M2(m2);
   matrix m3 = {{CNum(0,0), CNum(0,0)},{CNum(0,0), CNum(0,0)}};
-  Matrix M3(m3);
-  EXPECT_EQ(M1.subtract(M2), M3);
+  Matrix M1(m1);
+  Matrix M2(m2);
+  Matrix Expected(m3);
+  EXPECT_EQ(M1.subtract(M2), Expected);
 }
 
 TEST(MatrixTest, SubtractWrongDimension) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(1,1)},{CNum(2,2)}};
+  Matrix M1(m1);
   Matrix M2(m2);
   EXPECT_THROW(M1.subtract(M2), std::runtime_error);
 }
@@ -42,34 +42,34 @@ TEST(MatrixTest, SubtractWrongDimension) {
 // Matrix scalarMultiply(const CNum& cn2) const;
 TEST(MatrixTest, ScalarMultiplyZero) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(0,0), CNum(0,0)},{CNum(0,0), CNum(0,0)}};
-  Matrix M2(m2);
-  EXPECT_EQ(M1.scalarMultiply(CNum(0,0)), M2);
+  Matrix M1(m1);
+  Matrix Expected(m2);
+  EXPECT_EQ(M1.scalarMultiply(CNum(0,0)), Expected);
 }
 
 TEST(MatrixTest, ScalarMultiplyReal) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(2,2), CNum(4,4)},{CNum(6,6), CNum(8,8)}};
-  Matrix M2(m2);
-  EXPECT_EQ(M1.scalarMultiply(CNum(2,0)), M2);
+  Matrix M1(m1);
+  Matrix Expected(m2);
+  EXPECT_EQ(M1.scalarMultiply(CNum(2,0)), Expected);
 }
 
 TEST(MatrixTest, ScalarMultiplyImaginary) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(-2,2), CNum(-4,4)},{CNum(-6,6), CNum(-8,8)}};
-  Matrix M2(m2);
-  EXPECT_EQ(M1.scalarMultiply(CNum(0,2)), M2);
+  Matrix M1(m1);
+  Matrix Expected(m2);
+  EXPECT_EQ(M1.scalarMultiply(CNum(0,2)), Expected);
 }
 
 TEST(MatrixTest, ScalarMultiplyComplex) {
   matrix m1 = {{CNum(1,1), CNum(2,2)},{CNum(3,3), CNum(4,4)}};
-  Matrix M1(m1);
   matrix m2 = {{CNum(0,4), CNum(0,8)},{CNum(0,12), CNum(0,16)}};
-  Matrix M2(m2);
-  EXPECT_EQ(M1.scalarMultiply(CNum(2,2)), M2);
+  Matrix M1(m1);
+  Matrix Expected(m2);
+  EXPECT_EQ(M1.scalarMultiply(CNum(2,2)), Expected);
 }
 
 // Matrix matrixMultiply(const Matrix& m2) const;
